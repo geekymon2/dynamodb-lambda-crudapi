73dff51587f1f80e0282eccd1c577c5351a3c096
geekymon2 <geekymon2@gmail.com>
2024-07-08 17:29:16 +1000

    updated zip commands to use 7zip

--- lambda_deploy_function.bat
+++ lambda_deploy_function.bat
@@ -4,7 +4,8 @@ del .\package\dynamodb-lambda-crudapi.zip 2>nul
 echo "Build Lambda CRUD API Function"
 call npm install --production
 echo "Package Lambda CRUD API Function"
-powershell Compress-Archive @('package.json', 'index.js', 'src', 'node_modules') package/dynamodb-lambda-crudapi.zip
+Rem "7zip is required to create a compressed archive"
+7z a -tzip ./package/dynamodb-lambda-crudapi.zip package.json index.js src/ node_modules/
 
 echo "Deploy to AWS Lambda"
 aws lambda update-function-code --function-name dynamodb-lambda-crudapi --zip-file fileb://package/dynamodb-lambda-crudapi.zip
--- lambda_upload_s3.bat
+++ lambda_upload_s3.bat
@@ -4,7 +4,8 @@ del .\package\dynamodb-lambda-crudapi.zip 2>nul
 echo "Build Lambda CRUD API Function"
 call npm install --production
 echo "Package Lambda CRUD API Function"
-powershell Compress-Archive @('package.json', 'index.js', 'src', 'node_modules') package/dynamodb-lambda-crudapi.zip
+Rem "7zip is required to create a compressed archive"
+7z a -tzip ./package/dynamodb-lambda-crudapi.zip package.json index.js src/ node_modules/
 
 echo "Upload to S3"
 aws s3 cp ./package/ s3://cf-templates-998/ --recursive
