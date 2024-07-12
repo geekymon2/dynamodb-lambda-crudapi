4cd3d71703cb4c5078a8dd9738eef348ea99ce7c
geekymon2 <geekymon2@gmail.com>
2024-07-12 17:12:06 +1000

    update

--- deploy_scripts/lambda_deploy.sh
+++ deploy_scripts/lambda_deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+ENV=$1
+
+if [ -z "$ENV" ]; then
+    echo "Usage: ./lambda_deploy.sh <ENV>"
+    exit 1
+fi
+
 sh ./lambda_package.sh
 
 echo "Deploy to AWS Lambda"
-aws lambda update-function-code --function-name dynamodb-lambda-crudapi --zip-file fileb://../package/dynamodb-lambda-crudapi.zip
+aws lambda update-function-code --function-name dynamodb-lambda-crudapi-$ENV --zip-file fileb://../package/dynamodb-lambda-crudapi.zip
